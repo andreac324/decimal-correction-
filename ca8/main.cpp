32245758d4f6064a6c495dc7a72dc74f0880ab43
andreac324 <terisa32873@gmail.com>
2024-09-14 20:50:22 -0700

    Initial Commit

---
+++ ca8/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ca8
+//
+//  Created by Andrea d Cazares on 9/14/24.
+//
+
+#include <iostream> 
+using namespace std;
+int main (){
+//as 8
+double x=10;
+int y=3;
+double z=x/y;
+ cout<<z<<endl;
+return 0;
+}
